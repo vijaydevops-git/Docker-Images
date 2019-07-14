167e1dde3c6311abe35fb0da7f9eebe77c21f0e7
navaneethreddydevops <navaneethreddydevops@gmail.com>
2019-07-14 03:34:01 -0500

    adding the compose files

--- dockersetup/dockerinstallation.sh
+++ dockersetup/dockerinstallation.sh
@@ -6,3 +6,14 @@ yum install docker-ce docker-ce-cli containerd.io -y
 systemctl enable docker
 systemctl start docker
 docker swarm join --token SWMTKN-1-27cqo75hosc1ezi5bndd8ofsn0kyyvwb0aplyyiuhcginimjyr-4lnnurd2zsvmuxbj2kfjl8y9g 172.31.17.245:2377
+
+
+
+
+
+######This is for Reference:
+
+docker network create -d=bridge company
+docker network ls
+docker run --rm -d --name customer-api --network company swarmgs/customer
+docker run --rm -d --name balance-api --network company -p 4000:3000 -e MYWEB_CUSTOMER_API=customer-api:3000 swarmgs/balance
